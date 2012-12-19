8a6cfa1e2c627ddb1ce62fa0c0f06b5e19b1ae8e
Xuejie Xiao <xxuejie@gmail.com>
2012-12-19 09:07:00 -0500

    Print return value and errors, close issue #1

--- driver.c
+++ driver.c
@@ -8,7 +8,16 @@ mrb_state* driver_open()
 
 int driver_execute_string(mrb_state *mrb, const char *s)
 {
-  mrb_load_string(mrb, s);
+  mrb_value result = mrb_load_string(mrb, s);
+  if (mrb->exc) {
+    mrb_p(mrb, mrb_obj_value(mrb->exc));
+    mrb->exc = 0;
+  }
+  else {
+    /* no */
+    printf(" => ");
+    mrb_p(mrb, result);
+  }
 
   return 0;
 }
