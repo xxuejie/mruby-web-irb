675ed0ebff3c70c3b1844cf0f8d7273c7fbee534
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2013-03-01 21:34:43 -0500

    Update mruby irb build script

--- build.sh
+++ build.sh
@@ -5,11 +5,7 @@
 # https://github.com/xxuejie/webruby
 
 # Usage:
-# ./build.sh (path to webruby)
-
-# This is the default closure jar path on a mac installed using homebrew
-# Change it if you install it to other path
-CLOSURE_COMPILER=/usr/local/Cellar/closure-compiler/20120917/libexec/build/compiler.jar
+# ./build.sh (path to webruby) (path to emscripten)
 
 WEBRUBY_ROOT=$1
 if [ -z $WEBRUBY_ROOT ]; then
@@ -17,21 +13,18 @@ if [ -z $WEBRUBY_ROOT ]; then
     exit 1
 fi
 
+EMSCRIPTEN_ROOT=$2
+if [ -z $EMSCRIPTEN_ROOT ]; then
+    echo "Please provide the path to emscripten!"
+    exit 1
+fi
+
 cd `dirname ${BASH_SOURCE[0]}`
 SCRIPT_ROOT=`pwd`
 
-EMCC=$WEBRUBY_ROOT/modules/emscripten/emcc
-EMCC_FLAGS="-Werror-implicit-function-declaration -DMRB_USE_FLOAT"
+EMCC=$EMSCRIPTEN_ROOT/emcc
+EMCC_FLAGS="-Werror-implicit-function-declaration"
 EMCC_INCLUDES="-I$WEBRUBY_ROOT/modules/mruby/include"
-
-# build webruby
-cd $WEBRUBY_ROOT
-git submodule init && git submodule update && rake libmruby
-if [ $? -ne 0 ]; then
-    echo "Error occurs when building webruby project, exit"
-    exit 1
-fi
-cd $SCRIPT_ROOT
 
 # build driver
 $EMCC $EMCC_FLAGS $EMCC_INCLUDES -o $SCRIPT_ROOT/driver.o $SCRIPT_ROOT/driver.c
@@ -41,7 +34,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # build mruby.js(O2 will invoke closure compiler)
-$EMCC $EMCC_FLAGS -o $SCRIPT_ROOT/mruby.js $SCRIPT_ROOT/driver.o $WEBRUBY_ROOT/modules/mruby/build/emscripten/lib/libmruby.a -s EXPORTED_FUNCTIONS="['_driver_execute_string', '_driver_open', '_driver_close']" -O2
+$EMCC $EMCC_FLAGS -o $SCRIPT_ROOT/mruby.js $SCRIPT_ROOT/driver.o $WEBRUBY_ROOT/modules/mruby/build/emscripten/lib/libmruby.a -s EXPORTED_FUNCTIONS="['_driver_execute_string', '_driver_open', '_driver_close']" -O2 --closure 1
 if [ $? -ne 0 ]; then
     echo "Error occurs when building mruby.js, exit"
     exit 1
