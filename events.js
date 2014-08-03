c0885635ecac08c820f1cf8fee34036cf2145d29
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2014-08-03 16:44:05 +0800

    Add print level control via HTTP query parameter, fixes #8

--- events.js
+++ events.js
@@ -1,6 +1,21 @@
 (function () {
   var lines = [], printed = false, webruby, load_string_func;
 
+  // Taken from http://stackoverflow.com/a/901144
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  function getQueryLevel() {
+    var level = parseInt(getParameterByName('level')) || 2;
+    level = Math.min(2, level);
+    level = Math.max(0, level);
+    return level;
+  }
+
   window.Module = {};
   window.Module['print'] = function (x) {
     lines.push(x);
@@ -8,7 +23,7 @@
   };
 
   $(document).ready(function() {
-    webruby = new WEBRUBY({print_level: 2});
+    webruby = new WEBRUBY({print_level: getQueryLevel()});
 
     $("#submit-button").click(function() {
       lines = [];
